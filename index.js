79f40292bc88690060e0db0621bd81fa6720fd24
Pedro <rm88246@fiap.com.br>
2024-07-28 22:14:28 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,1 @@
+export { buildCanonical } from './src/buildCanonical';
---
+++ src/canonical-builder.js
@@ -0,0 +1,18 @@
+/**
+ * Função que recebe o canonical, o payload de carregamento e os dados processados e retorna o canonical modificado.
+ *
+ * @param {*} canonical 
+ * @param {*} payloadDeCarregamento 
+ * @param {*} processedData 
+ * @returns 
+ */
+export const buildCanonical = (canonical, payloadDeCarregamento, processedData) => {
+
+  if (!canonical) return;
+
+  switch (canonical.name) {
+    // faça algo com o canonico.
+  }
+
+  return data;
+}
